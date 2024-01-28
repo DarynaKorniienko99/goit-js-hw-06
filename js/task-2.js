41dc582245e3570c90926927b0e4f94523e99e0e
DarynaKorniienko99 <d.korniienko0907@gmail.com>
2024-01-28 19:45:59 +0100

    js.hw6

---
+++ js/task-2.js
@@ -0,0 +1,33 @@
+'use strict';
+class Storage {
+#items;
+
+constructor(items) {
+    this.#items = items;
+}
+
+getItems() {
+    return this.#items;
+}
+
+addItem(newItem) {
+    this.#items.push(newItem);
+}
+
+removeItem(itemToRemove) {
+    const indexItemRemove = this.#items.indexof(itemToRemove);
+    if (indexItemRemove !== -1) { 
+        this.#items.splice(indexItemRemove, 1);
+    }
+    else {
+        return 'This element is not found.';
+    }
+}
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
